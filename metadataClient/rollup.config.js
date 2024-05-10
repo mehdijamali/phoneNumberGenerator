49e8118446e5fd4e01f3b0406ea8820686c3c6ab
Mehdi <mehdi.jamali@nl.abb.com>
2024-05-10 12:35:40 +0200

    fix build issue for database client and meta data client

---
+++ metadataClient/rollup.config.js
@@ -0,0 +1,23 @@
+// rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json"; // Import the JSON plugin
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/bundle.js",
+    format: "es", // or 'cjs' depending on your target environment
+    sourcemap: true,
+  },
+  plugins: [
+    json(), // Add the JSON plugin to the plugins array
+    resolve({
+      extensions: [".js", ".ts"],
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.build.json" }),
+  ],
+};
