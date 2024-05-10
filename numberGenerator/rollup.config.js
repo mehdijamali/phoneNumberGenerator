7131090ef8dee5fd0fcb8413066cab9303aed605
Mehdi <mm.jamali@gmail.com>
2024-05-10 17:31:40 +0200

    add Readme.md

--- numberGenerator/rollup.config.js
+++ numberGenerator/rollup.config.js
@@ -1,25 +1,24 @@
-// rollup.config.js
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json"; // Import the JSON plugin
+import json from "@rollup/plugin-json";
 
 export default [
   {
     input: "src/index.ts",
     output: {
       file: "dist/bundle.js",
-      format: "es", // or 'cjs' depending on your target environment
+      format: "es",
       sourcemap: true,
     },
     plugins: [
-      json(), // Add the JSON plugin to the plugins array
+      json(),
       resolve({
         extensions: [".js", ".ts"],
         preferBuiltins: true,
       }),
       commonjs({
-        include: /node_modules/, // Ensure it includes node_modules,
+        include: /node_modules/,
         strictRequires: true,
       }),
       typescript({ tsconfig: "./tsconfig.build.json" }),
